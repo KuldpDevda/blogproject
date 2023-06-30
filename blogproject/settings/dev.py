3e0e7f58aae2bfa0810ee9d77366fb40868c8d0c
kulddep <kuldeep@thoughtwin.com>
2023-06-30 16:20:05 +0530

    fix: authentications issue on psql

--- blogproject/settings/dev.py
+++ blogproject/settings/dev.py
@@ -4,7 +4,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blogproject',
-        'USER': 'postgres',
+        'USER': 'postgres1',
         'PASSWORD': 'psql',
         'HOST': 'localhost',
         'PORT': '5432',
