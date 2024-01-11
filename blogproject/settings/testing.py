4c5fd7d8f2d8b72cd200243d2e5b51880e182d3f
kulddep <kuldeep@thoughtwin.com>
2024-01-11 11:37:20 +0530

    add new file testing

---
+++ blogproject/settings/testing.py
@@ -0,0 +1,16 @@
+from .base import *
+
+
+ROOT_URLCONF = 'blogproject.urls'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
+}
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
+
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
